0c7a859f97725e05e5616f779c144b13a973b170
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-06-27 12:47:50 +0200

    Changed the frame size

--- src/Frame.java
+++ src/Frame.java
@@ -1,6 +1,6 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
@@ -8,6 +8,7 @@ public class Frame{
 
     private static JFrame frame = new JFrame();
     private static JPanel imagePanel = new JPanel();
+    private static JLabel imageLabel = new JLabel();
     private static JButton chooseImageButton = new JButton();
     private static JButton cutImageButton = new JButton();
     private static BufferedImage image;
@@ -24,15 +25,15 @@ public class Frame{
         imagePanel.setBackground(new java.awt.Color(153, 153, 153));
         imagePanel.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
-        javax.swing.GroupLayout ImagePanelLayout = new javax.swing.GroupLayout(imagePanel);
-        imagePanel.setLayout(ImagePanelLayout);
-        ImagePanelLayout.setHorizontalGroup(
-                ImagePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 0, Short.MAX_VALUE)
+        javax.swing.GroupLayout imagePanelLayout = new javax.swing.GroupLayout(imagePanel);
+        imagePanel.setLayout(imagePanelLayout);
+        imagePanelLayout.setHorizontalGroup(
+                imagePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(imageLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
-        ImagePanelLayout.setVerticalGroup(
-                ImagePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 319, Short.MAX_VALUE)
+        imagePanelLayout.setVerticalGroup(
+                imagePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(imageLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 720, Short.MAX_VALUE)
         );
 
         chooseImageButton.setText("Choose Image");
@@ -51,7 +52,7 @@ public class Frame{
                                         .addComponent(imagePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                         .addGroup(layout.createSequentialGroup()
                                                 .addComponent(chooseImageButton, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 276, Short.MAX_VALUE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 911, Short.MAX_VALUE)
                                                 .addComponent(cutImageButton, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)))
                                 .addContainerGap())
         );
@@ -61,8 +62,8 @@ public class Frame{
                                 .addContainerGap()
                                 .addComponent(imagePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(chooseImageButton, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(chooseImageButton, javax.swing.GroupLayout.DEFAULT_SIZE, 49, Short.MAX_VALUE)
                                         .addComponent(cutImageButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                 .addContainerGap())
         );
@@ -81,5 +82,9 @@ public class Frame{
         fileChooser.showOpenDialog(null);
         File selectedFile = fileChooser.getSelectedFile();
         return selectedFile;
+    }
+
+    private static void renderImage(BufferedImage image){
+
     }
 }
